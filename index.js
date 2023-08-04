afa2937d45bda159d0deb42e79d43b620ebef9cd
Kelvin M Matheka <beingmatheka@gmail.com>
2023-08-04 21:06:06 +0300

    index.js

--- index.js
+++ index.js
@@ -13,3 +13,54 @@ app.use(cors());
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+let users = [];
+
+// API Endpoints
+app.post('/api/register', (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Please provide both username and password.' });
+  }
+
+  // Check if the user already exists
+  if (users.some((user) => user.username === username)) {
+    return res.status(409).json({ error: 'User already exists.' });
+  }
+
+  // Store the new user in the in-memory data
+  users.push({ username, password, balance: 0 });
+  res.json({ message: 'User registered successfully.' });
+});
+
+app.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find((user) => user.username === username && user.password === password);
+  if (!user) {
+    return res.status(401).json({ error: 'Invalid username or password.' });
+  }
+  res.json({ message: 'Login successful.', username: user.username });
+});
+
+app.post('/api/transfer', (req, res) => {
+  const { sender, recipient, amount } = req.body;
+  const senderUser = users.find((user) => user.username === sender);
+  const recipientUser = users.find((user) => user.username === recipient);
+  
+  if (!senderUser || !recipientUser) {
+    return res.status(404).json({ error: 'Sender or recipient not found.' });
+  }
+
+  if (amount <= 0) {
+    return res.status(400).json({ error: 'Amount must be greater than zero.' });
+  }
+
+  if (senderUser.balance < amount) {
+    return res.status(403).json({ error: 'Insufficient funds.' });
+  }
+
+  senderUser.balance -= amount;
+  recipientUser.balance += amount;
+
+  res.json({ message: 'Money transferred successfully.' });
+});
