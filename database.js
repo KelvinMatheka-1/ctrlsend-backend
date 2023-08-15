64faa46173bec0a9f2383984e0a91b1f51957d73
Kelvin M Matheka <beingmatheka@gmail.com>
2023-08-15 17:17:27 +0300

    configuring aws

--- database.js
+++ database.js
@@ -1,20 +1,20 @@
-const {Client} = require('pg')
+// const {Client} = require('pg')
 
-const client = new Client({
-  host: 'localhost',
-  user: 'kelvin',
-  port: 5432,
-  pasword: "Omarionconor2",
-  database: "postgres"
-})
+// const client = new Client({
+//   host: 'localhost',
+//   user: 'kelvin',
+//   port: 5432,
+//   pasword: "Omarionconor2",
+//   database: "postgres"
+// })
 
-client.connect();
+// client.connect();
 
-client.query(`Select * from users`, (res, err) => {
-  if(!err){
-    console.log(res.rows);
-  }else{
-    console.log(err.message);
-  }
-  client.end;
-})
+// client.query(`Select * from users`, (res, err) => {
+//   if(!err){
+//     console.log(res.rows);
+//   }else{
+//     console.log(err.message);
+//   }
+//   client.end;
+// })
