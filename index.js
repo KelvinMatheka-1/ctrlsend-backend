b1059ddb8d2e17271272b425957ec33c21971839
Kelvin M Matheka <beingmatheka@gmail.com>
2023-10-20 23:26:28 +0300

    server

--- index.js
+++ index.js
@@ -544,3 +544,7 @@ app.delete("/api/users/:userId", requireAuth, async (req, res) => {
   }
 });
 
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
